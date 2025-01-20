6b63e877dc91244cc7600dbf7322388c13f810a6
yuecelem <emre.ycl@hotmail.com>
2025-01-20 18:02:46 +0300

    Add many things

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ const gameBoard = (() => {
         [0, 0, 0]
     ];
 
-    function markBoard(cell, symbol) {
+    function markCell(cell, symbol) {
         board[cell[0]][cell[1]] = symbol;
     }
 
     function checkCell(cell) {
-        return (board[cell[0]][cell[1]] !== 0)
+        return (board[cell[0]][cell[1]] == '0')
     }
-    return { board, markBoard, checkCell }
-})
 
-const player = (() => {
+    function getBoard () {
+        return board
+    }
+
+    return { getBoard, markCell, checkCell }
+})();
+
+const player = ((name, symbol) => {
 
     const playerInfo = {
-        name: 'name',
-        symbol: 'symbol',
+        name: name,
+        symbol: symbol,
     }
 
     function setPlayerName(newName) {
@@ -30,43 +35,136 @@ const player = (() => {
         playerInfo.symbol = newSymbol;
     }
 
-    return { playerInfo, setPlayerName, setPlayerSymbol }
-})
-
-const playGame = (() => {
-    let lastPlayer = [];
-    const board = gameBoard();
-    const player1 = player();
-    const player2 = player();
-
-    console.log("We're Here!")
-    console.log("Board", board)
-    console.log("P1", player1)
-    console.log("P2", player2)
-    console.log(player1.playerInfo)
-    player1.playerInfo.name = 'memo'
-
-
-    function playerTurnAlternator(){
-        if(lastPlayer === player1.playerInfo.name) {
-            lastPlayer = player2.playerInfo.name;
-        } else lastPlayer = player1.playerInfo.name;
-        
+    function getPlayerInfo () {
+        return playerInfo
     }
 
 
-    return {player1, player2, playerTurnAlternator, lastPlayer}
+    return { getPlayerInfo, setPlayerName, setPlayerSymbol }
+})
+
+const playGame = ((board, player1, player2) => {
+    let winner = '';
+
+    function playRound(player, cellLocation) {
+        if (board.checkCell(cellLocation)) {
+            board.markCell(cellLocation, player.getPlayerInfo().symbol)
+        } else {
+            console.log("Cell is occupied")
+        }}
+
+    function setWinner() {
+
+        let boardState = board.getBoard();
+
+        //Rows
+        if(boardState[0][0] === 'X' && boardState[0][1] === 'X' && boardState[0][2] === 'X'){
+            winner = 'X';
+            return winner
+ 
+        } else if (boardState[1][0] === 'X' && boardState[1][1] === 'X' && boardState[1][2] === 'X') {
+            winner = 'X';
+            return winner
+ 
+        } else if (boardState[2][0] === 'X' && boardState[2][1] === 'X' && boardState[2][2] === 'X') {
+            winner = 'X';
+            return winner
+ 
+        } else if(boardState[0][0] === 'O' && boardState[0][1] === 'O' && boardState[0][2] === 'O'){
+            winner = 'O';
+            return winner
+ 
+        } else if (boardState[1][0] === 'O' && boardState[1][1] === 'O' && boardState[1][2] === 'O') {
+            winner = 'O';
+            return winner
+ 
+        } else if (boardState[2][0] === 'O' && boardState[2][1] === 'O' && boardState[2][2] === 'O') {
+            winner = 'O';
+            return winner
+ 
+        //Columns
+        } else if(boardState[0][0] === 'X' && boardState[1][0] === 'X' && boardState[2][0] === 'X'){
+            winner = 'X';
+            return winner
+ 
+        } else if (boardState[0][1] === 'X' && boardState[1][1] === 'X' && boardState[2][1] === 'X') {
+            winner = 'X';
+            return winner
+ 
+        } else if (boardState[0][2] === 'X' && boardState[1][2] === 'X' && boardState[2][2] === 'X') {
+            winner = 'X';
+            return winner
+ 
+        } else if(boardState[0][0] === 'O' && boardState[1][0] === 'O' && boardState[2][0] === 'O'){
+            winner = 'O';
+            return winner
+ 
+        } else if (boardState[0][1] === 'O' && boardState[1][1] === 'O' && boardState[2][1] === 'O') {
+            winner = 'O';
+            return winner
+ 
+        } else if (boardState[0][2] === 'O' && boardState[1][2] === 'O' && boardState[2][2] === 'O') {
+            winner = 'O';
+            return winner
+
+        } 
+        //Diagonal
+        else if(boardState[0][0] === 'X' && boardState[1][1] === 'X' && boardState[2][2] === 'X'){
+            winner = 'X';
+            return winner
+
+
+        } else if (boardState[0][2] === 'X' && boardState[1][1] === 'X' && boardState[2][0] === 'X') {
+            winner = 'X';
+            return winner
+
+            
+        } else if(boardState[0][0] === 'O' && boardState[1][1] === 'O' && boardState[2][2] === 'O'){
+            winner = 'O';
+            return winner
+
+        } else if (boardState[0][2] === 'O' && boardState[1][1] === 'O' && boardState[2][0] === 'O') {
+            winner = 'O';
+            return winner
+        } else return null
+
+    }
+
+    function getWinner () {
+        return winner
+    }
+
+
+    return { playRound, setWinner, getWinner }
 
 
 })
 
-const game = playGame()
-console.log("P1 name:" + game.player1.playerInfo.name)
-console.log("P2 name:" + game.player2.playerInfo.name)
-console.log(game.lastPlayer)
+function initGame () {
+    console.log(gameBoard.getBoard())
+}
 
-game.playerTurnAlternator()
-console.log("P1 name:" + game.player1.playerInfo.name)
-console.log("P2 name:" + game.player2.playerInfo.name)
-console.log(game.lastPlayer)
+initGame()
+
+
+
+// console.log(gameBoard.getBoard())
+
+// const player1 = player('Mahmut', 'X');
+// console.log(player1.getPlayerInfo())
+
+// const player2 = player('Anan', 'O');
+// console.log(player2.getPlayerInfo())
+
+// const newGame = playGame(gameBoard, player1, player2)
+
+// newGame.playRound(player1, [0,0])
+// newGame.playRound(player1, [0,1])
+// newGame.playRound(player1, [0,2])
+
+// console.log(gameBoard.getBoard())
+
+
+
+
 
